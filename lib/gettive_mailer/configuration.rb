2e1f90a3fe64b6891ab900bc44b7b123fb4ab5ba
Vincent <makokhavomondi@gmail.com>
2026-07-06 23:06:51 +0300

    Set up our actual server location

--- lib/gettive_mailer/configuration.rb
+++ lib/gettive_mailer/configuration.rb
@@ -3,8 +3,7 @@ module GettiveMailer
         attr_accessor :api_key, :endpoint
 
         def initialize
-            # @endpoint = "http://127.0.0.1:3000"
-            @endpoint = "https://webhook.site/5b1798e7-3d89-496b-97d8-2a81c60a1296"
+            @endpoint = "https://api.mailer.gettive.com"
         end
     end
 end
