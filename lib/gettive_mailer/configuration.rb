2876607e746978622d5400a1ef86260d50d506ea
Vincent <makokhavomondi@gmail.com>
2026-07-17 05:26:47 +0300

    Update default endpoint

--- lib/gettive_mailer/configuration.rb
+++ lib/gettive_mailer/configuration.rb
@@ -3,7 +3,7 @@ module GettiveMailer
         attr_accessor :api_key, :endpoint
 
         def initialize
-            @endpoint = ENV.fetch("GETTIVE_MAILER_ENDPOINT", "https://api.mailer.gettive.com")
+            @endpoint = ENV.fetch("GETTIVE_MAILER_ENDPOINT", "https://tuqfgkz924.execute-api.us-east-1.amazonaws.com")
         end
     end
 end
