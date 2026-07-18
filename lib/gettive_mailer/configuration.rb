83638676afd9642fc9e710400127d14f51e64673
Vincent <makokhavomondi@gmail.com>
2026-07-18 14:23:22 +0300

    Undo default configurations

--- lib/gettive_mailer/configuration.rb
+++ lib/gettive_mailer/configuration.rb
@@ -1,9 +1,5 @@
 module GettiveMailer
     class Configuration
         attr_accessor :api_key, :host
-
-        def initialize
-            @host = ENV.fetch("GETTIVE_MAILER_HOST", "https://tuqfgkz924.execute-api.us-east-1.amazonaws.com")
-        end
     end
 end
